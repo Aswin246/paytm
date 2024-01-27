1a9bbe3bf13d99eba577b03c1a1c2397b16b816f
Aswin246 <mashwinarjun@gmail.com>
2024-01-27 20:54:30 +0530

    Backend completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <div>
-        Hello world
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/signin" element={<SigInp />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/send" element={<SendMoney />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
