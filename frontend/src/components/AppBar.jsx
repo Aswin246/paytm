812ec6d6250c583e2310f5c7702ed7c5088d99f5
Aswin246 <mashwinarjun@gmail.com>
2024-01-30 21:32:35 +0530

    More frontend added

--- frontend/src/components/AppBar.jsx
+++ frontend/src/components/AppBar.jsx
@@ -1,19 +1,38 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export const Appbar = () => {
+  const [firstName, setFirstName] = useState("");
+
+  useEffect(() => {
+    const fetchName = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3001/api/v1/user/name",
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem(token)}`,
+            },
+          }
+        );
+        setFirstName(response.data.firstName);
+        console.log(firstName);
+      } catch (error) {
+        console.error("Error fetching name:", error);
+      }
+    };
+    fetchName();
+  }, []);
+
   return (
     <div className="shadow h-14 flex justify-between">
-    <div className="flex flex-col justify-center h-full ml-4">
-        PayTM App
-    </div>
-    <div className="flex">
+      <div className="flex flex-col justify-center h-full ml-4">PayTM App</div>
+      <div className="flex">
         <div className="flex flex-col justify-center h-full mr-4">
-            Hello
+          {firstName}
         </div>
-        <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
-            <div className="flex flex-col justify-center h-full text-xl">
-                U
-            </div>
-        </div>
+        <div>{firstName}</div>
+      </div>
     </div>
-</div>
   );
 };
